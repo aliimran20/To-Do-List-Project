2bf1b21490f8ac45cd4ccbc2eb265ae0ce8fb449
Ali Imran <iali81h@gmail.com>
2024-04-20 16:04:18 +0500

    Project Added

---
+++ index.ts
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+async function main() 
+
+{
+
+    const tasks: string[] = []
+
+    console.log(chalk.red('\nWELCOME TO TO-DO APP!'))
+
+    while (true) 
+    
+    {
+
+        const { task } = await inquirer.prompt
+        
+        ({
+
+            name: 'task',
+            type: 'input',
+            message: chalk.yellow('\nPLEASE ENTER YOUR TO-DO TASK:'),
+            prefix: ''
+
+        })
+
+        tasks.push(task.toUpperCase()) //converts all added tasks to upper case
+
+        const { moreTasks } = await inquirer.prompt
+        
+        ({
+
+            name: 'moreTasks',
+            type: 'confirm',
+            message: chalk.yellow('\nDO YOU HAVE MORE TASKS TO ADD?'),
+            prefix: ''
+
+        })
+
+        if (!moreTasks) 
+        
+        break //breaks the operation if there are no more tasks
+        
+    }
+
+    console.log (chalk.blue('\nYOUR ENTERED TASKS ARE:\n'))
+
+    tasks.forEach((task, index) => 
+    
+    {
+
+        console.log (chalk.magenta(`${index + 1}. ${task}`)) //prints the next index without disturbing the previous one
+
+    })
+
+}
+
+main()
